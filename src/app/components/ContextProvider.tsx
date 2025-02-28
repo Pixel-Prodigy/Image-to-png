65386e8353e997d84a4307b5e3aeb30624f58a51
Pixel-Prodigy <gurumarkanyt@gmail.com>
2025-02-28 18:33:25 +0530

    Updated: src/app/components/ContextProvider.tsx

--- src/app/components/ContextProvider.tsx
+++ src/app/components/ContextProvider.tsx
@@ -1,9 +1,29 @@
-"use client"
-import React, { useState } from 'react'
-import { Context } from './Context'
-export default function ContextProvider({children}:{children: React.ReactNode}) {
-  const [imgValue , setImageValue] = useState<File | null>(null)
-    return (
-    <Context.Provider value={{imgValue , setImageValue}}>{children}</Context.Provider>
-  )
+"use client";
+import React, { useState } from "react";
+import { Context } from "./Context";
+export default function ContextProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [imgValue, setImageValue] = useState<File | null>(null);
+  const [preview, setPreview] = useState<string | undefined>(undefined);
+  const [convertedUrl, setConvertedUrl] = useState<Blob | null>(null);
+  const [copyCode, setCopyCode] = useState<string | null>(null);
+  return (
+    <Context.Provider
+      value={{
+        imgValue,
+        setImageValue,
+        setPreview,
+        preview,
+        setConvertedUrl,
+        convertedUrl,
+        copyCode,
+        setCopyCode,
+      }}
+    >
+      {children}
+    </Context.Provider>
+  );
 }
