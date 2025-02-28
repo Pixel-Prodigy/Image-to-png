4f657467094391d25b9aa3b46da496ca81608f14
Pixel-Prodigy <gurumarkanyt@gmail.com>
2025-02-28 18:33:25 +0530

    Updated: src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,12 @@
 "use client";
-import IntroBox from "./components/IntroBox";
 import Heading from "./components/Heading";
-import FinalBox from "./components/FinalBox";
-
+import IntroBox from "./components/IntroBox";
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-start">
-      <Heading />
-      <div className="grid grid-cols-1 mt-20 h-[400px] place-items-center max-w-[800px] w-full">
-        <FinalBox />
+    <div className="flex relative flex-col items-center justify-start overflow-x-hidden h-fit min-h-screen">
+      <Heading text="Image" xClass="text-[12rem] " textClass="text-8xl" />
+      <div className="grid pb-10 place-items-center h-[400px] max-w-[800px] w-full">
+        <IntroBox />
       </div>
     </div>
   );
