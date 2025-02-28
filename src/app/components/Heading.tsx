9044eb6aec63c2470dc47760909bcb30ffbed85a
Pixel-Prodigy <gurumarkanyt@gmail.com>
2025-02-28 18:33:25 +0530

    Updated: src/app/components/Heading.tsx

--- src/app/components/Heading.tsx
+++ src/app/components/Heading.tsx
@@ -1,23 +1,23 @@
 import React from "react";
 import { motion, useTransform, useScroll } from "framer-motion";
-export default function Heading() {
+export default function Heading({text , textClass , xClass}:{text: string , textClass?: string , xClass?: string}) {
   return (
     <>
       <motion.div className="mt-20">
         <motion.h1
           transition={{ duration: 3 }}
-          className="text-6xl font-bold text-center font-mono flex items-center justify-center"
+          className=" font-bold text-center font-mono flex items-center justify-center"
         >
           <motion.span
             initial={{ color: "#f29296", scale: 1.4 }}
             animate={{ color: "#ffffff", scale: 1 }}
             transition={{ type: "spring ",  stiffness: 300, damping: 30 }}
-            className="text-8xl"
+            className={` ${textClass}`}
           >
-            Image
+            {text}
           </motion.span>
           <motion.span
-            className="text-[12rem] ml-2"
+            className={` ml-2 ${xClass}`}
             initial={{ color: "#ffffff", scale: 0.4 }}
             animate={{ color: "#f29296", scale: 1 }}
             transition={{ type: "spring ",  stiffness: 300, damping: 30 }}
