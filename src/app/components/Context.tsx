2fc315fcbd8f9f3864334bc804638b29282297a4
Pixel-Prodigy <gurumarkanyt@gmail.com>
2025-02-28 18:33:25 +0530

    Updated: src/app/components/Context.tsx

--- src/app/components/Context.tsx
+++ src/app/components/Context.tsx
@@ -3,5 +3,11 @@ import { createContext } from "react";
 type Types = {
   imgValue: File | null;
   setImageValue: React.Dispatch<React.SetStateAction<File | null>>;
+  preview: string | undefined;
+  setPreview: React.Dispatch<React.SetStateAction<string | undefined>>;
+  convertedUrl: Blob | null;
+  setConvertedUrl: React.Dispatch<React.SetStateAction<Blob | null>>;
+  copyCode: string | null
+  setCopyCode:  React.Dispatch<React.SetStateAction<string | null>>;
 };
 export const Context = createContext<Types | null>(null);
